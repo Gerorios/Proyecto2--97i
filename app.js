7e19f0b96cc82911b7ac71e7881a8d2e4766ac64
gero rios <antenuccifc87@gmail.com>
2024-05-11 20:31:33 -0300

    Retoques en el nav , cambios de color y ajuste de efectos

--- app.js
+++ app.js
@@ -2,6 +2,4 @@ const toggleBtn = document.querySelector(".toggle-btn")
 const dropdownMenu = document.querySelector(".dropdown_menu")
 const openMenu = () =>{
    dropdownMenu.classList.toggle("open")
-
-
 }
